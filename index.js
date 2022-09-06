6e30e194ca6f21d5f8732f2edd8c143d9fff650f
Coffee58511867 <marobahlalele910@gmail.com>
2022-09-06 08:50:00 +0200

    Created Pern Backend

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
-app.post("/add", async (res, req) => {
+app.post("/add", async (req, res) => {
   try {
-    const { description } = req.body;
+    const  {description} = req.body
     const newtodo = await pool.query(
-      "INSERT INTO todo (description) VALUES ($1) RETURNING * ",
-      [description]
+      "INSERT INTO todo (description) VALUES ($1) RETURNING * ", [description]
     );
     res.json(newtodo.rows[0]);
   } catch (err) {
     console.log(err.message);
   }
 });
-app.get("/read", async (res, req) => {
+app.get("/read", async (req,res) => {
   try {
     const data = await pool.query("SELECT * FROM todo");
     res.json(data.rows);
@@ -26,9 +25,9 @@ app.get("/read", async (res, req) => {
     console.log(err.message);
   }
 });
-app.get("/read/:id", async (res, req) => {
+app.get("/read/:id", async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     const todoItem = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
       id
     ]);
@@ -37,10 +36,10 @@ app.get("/read/:id", async (res, req) => {
     console.log(err.message);
   }
 });
-app.put("/update/:id", async(res,req) => {
+app.put("/update/:id", async(req,res) => {
     try{
-       const {id} = req.params;
-       const {description} = req.body;
+       const {id} = req.params
+       const {description} = req.body
        const updateTodo = await pool.query("UPDATE todo set description = $1 WHERE todo_id = $2",
         [description,id]);
       res.json("Todo Updated");
@@ -49,9 +48,9 @@ app.put("/update/:id", async(res,req) => {
         console.log(err.message);
     }
 })
-app.delete("/delete/:id", async (res, req) => {
+app.delete("/delete/:id", async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     const todoItem = await pool.query("DELETE FROM todo WHERE todo_id = $1", 
     [
       id
@@ -61,7 +60,7 @@ app.delete("/delete/:id", async (res, req) => {
     console.log(err.message);
   }
 });
-app.delete("/deleteall", async (res, req) => {
+app.delete("/deleteall", async (req,res) => {
     try {
       const todoItem = await pool.query("DELETE * FROM todo");
       res.json("All Todos are Deleted");
